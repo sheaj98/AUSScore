7a08ab3b5e1783af04cca5fbe132c7d86b1638f3
Shea Sullivan <sheaj98@gmail.com>
2022-12-12 20:14:46 -0400

    Initial Commit

---
+++ AUSScore/AUSScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AUSScoreApp.swift
+//  AUSScore
+//
+//  Created by Shea Sullivan on 2022-12-12.
+//
+
+import SwiftUI
+
+@main
+struct AUSScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
