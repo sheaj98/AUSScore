e6886b39933dfb58cc2f533cda5f0d6663f86826
Cory Sullivan <cory@solbits.com>
2022-12-30 13:21:03 -0400

    open links externally

--- AUSScoreCore/Sources/NewsFeature/ArticleView.swift
+++ AUSScoreCore/Sources/NewsFeature/ArticleView.swift
@@ -6,6 +6,7 @@ import SwiftUI
 import WebKit
 
 // MARK: - ArticleFeature
+
 // This is a known issue with WKWebView
 // Error: [Security] This method should not be called on the main thread as it may lead to UI unresponsiveness.
 // See:https://developer.apple.com/forums/thread/714467?answerId=734799022#734799022
@@ -84,15 +85,16 @@ final class Coordinator: NSObject, WKUIDelegate, WKNavigationDelegate {
   // MARK: WKNavigationDelegate
 
   @MainActor
-  func webView(_: WKWebView, decidePolicyFor _: WKNavigationAction) async -> WKNavigationActionPolicy {
-    // The first request returns a URL of "about:blank", indicating a blank page probably because we are loading this from an HTML
-    // string.
-    // Therefore if the scheme is "about" we will allow this request. Otherwise, open safari to view other links.
-//    guard let url = navigationAction.request.url else { return .allow }
-//    guard url.scheme != "about" else { return .allow }
-//    _ = await UIApplication.shared.open(url)
-//    return .cancel
-    .allow
+  func webView(_: WKWebView, decidePolicyFor navigationAction: WKNavigationAction) async -> WKNavigationActionPolicy {
+    // If the user taps a link in the article we would like to open up the link in the users default browser
+    guard let url = navigationAction.request.url else { return .allow }
+    switch navigationAction.navigationType {
+    case .linkActivated:
+      _ = await UIApplication.shared.open(url)
+      return .cancel
+    default:
+      return .allow
+    }
   }
 
   func webView(_: WKWebView, didFinish _: WKNavigation!) {
